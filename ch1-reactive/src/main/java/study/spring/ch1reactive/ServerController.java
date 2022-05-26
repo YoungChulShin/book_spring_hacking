c59c5a0c4102258c4b8cad43e1cbdc38c87600d3
YoungChulShin <go1323@gmail.com>
2022-05-26 23:13:35 +0900

    ServerController 추가

---
+++ ch1-reactive/src/main/java/study/spring/ch1reactive/ServerController.java
@@ -0,0 +1,20 @@
+package study.spring.ch1reactive;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerController {
+
+  private final KitchenService kitchen;
+
+  public ServerController(KitchenService kitchen) {
+    this.kitchen = kitchen;
+  }
+
+  @GetMapping(value = "/server", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  Flux<Dish> serveDishes() {
+    return this.kitchen.getDishes();
+  }
+}
