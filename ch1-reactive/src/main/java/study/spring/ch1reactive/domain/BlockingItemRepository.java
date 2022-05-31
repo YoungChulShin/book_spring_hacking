dff1e9c89105219486aa02d0b8e8f9cdf18af423
YoungChulShin <go1323@gmail.com>
2022-06-01 06:53:30 +0900

    BlockingItemRepository 삭제

--- ch1-reactive/src/main/java/study/spring/ch1reactive/domain/BlockingItemRepository.java
+++
@@ -1,7 +0,0 @@
-package study.spring.ch1reactive.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BlockingItemRepository extends CrudRepository<Item, String> {
-
-}
